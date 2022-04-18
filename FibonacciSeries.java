88a49f72e10838c6985d0c9ccbf3598ae8629e08
RIZVANSHERIFFGA <103945205+RIZVANSHERIFFGA@users.noreply.github.com>
2022-04-18 20:14:04 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+package week1.day1.assignments;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 8;
+		
+		for ( i = 8; i >= 8; i++);{
+			System.out.println(i++);
+			
+		}
+
+	}
+		
+
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+package week1.day1.assignments;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 13;
+		int b = 2;
+		boolean i = (a < b);
+				
+		System.out.println(" " + i);
+		
+		boolean j = (a==b);
+		System.out.println(""+j);
+		
+		boolean k = (a != b);
+		System.out.println(k);
+		
+	 System.out.println(a%b);
+		
+		
+	
+			
+		}
+		
+		
+	}
+
